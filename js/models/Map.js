13c412852c4ac69d293b9c3492eef86a4298b752
Jerónimo <jeronimo.taborda63057@ucaldas.edu.co>
2026-02-25 21:44:26 -0500

    Merging to main

--- js/models/Map.js
+++ js/models/Map.js
@@ -1,37 +1,37 @@
-class Map{
-    constructor(width, height, cells){
-        this._width=width || null;
-        this.height=height || null;
-        this._cells=cells || [];
+class Map {
+    constructor(width, height, cells) {
+        this._width = width || null;
+        this.height = height || null;
+        this._cells = cells || [];
     }
     //==========GETTERS=============
-    get width(){
+    get width() {
         return this._width;
     }
-    get height(){
+    get height() {
         return this._height;
     }
-    get cells(){
+    get cells() {
         return this._cells;
     }
     //==========SETTERS=============
-    set width(width){
-        if(width >=0){
-            this._width=width;
+    set width(width) {
+        if (width >= 0) {
+            this._width = width;
         }
     }
-    set height(height){
-        if(height >=0){
-            this._height=height;
+    set height(height) {
+        if (height >= 0) {
+            this._height = height;
         }
     }
-    set cells(cells){
-        if(cells instanceof Array){
-            this._cells=cells;
+    set cells(cells) {
+        if (cells instanceof Array) {
+            this._cells = cells;
         }
     }
-    isEmpty(x,y){
-        if(x==null || y==null){
+    isEmpty(x, y) {
+        if (x == null || y == null) {
             return false;
         }
     }
