1c197db34688e4e90e08d39665994a08a1b4db57
Juan Felipe Sanabria <juan.sanabria64903@ucaldas.edu.co>
2026-03-02 11:50:09 -0500

    Changing SaveManager

--- js/managers/BuildingManager.js
+++ js/managers/BuildingManager.js
@@ -1,7 +1,16 @@
-class BuildingManager{
-    constructor(){
+class BuildingManager {
+    constructor() {
         this._buildings = []
     }
 
-    addBui
+    createBuilding(building) {
+        this._buildings.push(building)
+    }
+
+    deleteBuilding(id) {
+        for (building in this._buildings) {
+            this._buildings.filter(building => building !== id)
+        }
+    }
+
 }
--- js/managers/SaveManager.js
+++ js/managers/SaveManager.js
@@ -1,3 +1,36 @@
 class SaveManager{
-    
+
+    static key = "city"
+
+    static saveGame(city){
+        localStorage.setItem(this.key, JSON.stringify(city));
+    }
+2
+    static loadGame(){
+        let data = localStorage.getItem(this.key);
+        if (!data) return [];
+
+        let cityPlane = JSON.parse(data)
+        
+        city = new City(
+                cityPlane.name,
+                cityPlane.mayor,
+                cityPlane.regionLat,
+                cityPlane.regionLon,
+                cityPlane.width,
+                cityPlane.height,
+                cityPlane.score,
+                cityPlane.hapinessAverage,
+                cityPlane.grid
+        );
+
+        let citizens = cityPlane.citizenManager.population.map(e=>
+            citizen = new Citizen(
+                e.id
+            );
+            citizen.happiness = e.happiness;
+        )
+        
+
+    }
 }
