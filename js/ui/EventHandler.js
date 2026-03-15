d2b249be175d2c81e427581947598cd877f513fc
Juan Felipe Sanabria <juan.sanabria64903@ucaldas.edu.co>
2026-03-15 01:08:59 -0500

    Añadiendo explicacion de funcion loadCities()

--- js/ui/EventHandler.js
+++ js/ui/EventHandler.js
@@ -14,25 +14,39 @@ document.addEventListener("DOMContentLoaded", () => {
     let inputRegion = document.getElementById('input-region')
     const cityRepository = new CityRepository();
 
+    //Funcion cargar ciudades para seleccionar ciudad en Region Geográfica
+    //Muchachos, esta parte sirve para después hacer conexión a las APIS de News y Weather, y para mostrar ciudades. Voy a intentar explicarlo lo más facil posible.
+    //Aclaro, esta función se llama al iniciar la pantalla de Crear Ciudad, está en las líneas 65-67.
     function loadCities(){
+        //Se cambia el option en index.html para mostrar mensaje de cargando ciudades
         inputRegion.innerHTML = '<option value="">— Cargando ciudades —</option>';
+
+        //Se obtienen las ciudades del objeto cityRepository
         cityRepository.getCities()
             .then(function(cities){
+                //Se organizan las ciudades en orden alfabético
                 cities.sort(function(city1,city2){
+                    //Funcion que retorna una comparacion entre dos iteradores. En este caso, como son ciudades, retorna el orden entre city1 y city2 con una funcion interna llamada localeCompare.
                     return city1.name.localeCompare(city2.name);
                 })
-
+                //Cuando ya se hayan organizado las ciudades, se cambia la etiqueta option en index.html para que muestre mensaje de Selecciona una ciudad.
                 inputRegion.innerHTML = '<option value="">— Selecciona una ciudad —</option>';
 
+                //Se iteran todas las ciudades(ya en orden alfabetico)
                 cities.forEach(function(city){
+                    //Se crea un elemento llamado option(sencillo de asimilar, ya que se estan SELECCIONANDO ciudades)
                     let option = document.createElement('option')
+                    //Para este elemento, se le asignan atributos a partir del id, nombre, latitud y longitud de cada ciudad.
+                    //Posteriormente, (no en esta parte de codigo, es una aclaración), latitud y longitud se usan en NewsServices y WeatherServices para mostrar las noticias y el clima de la región geográfica que uno selecciona.
                     option.value = city.id;
                     option.textContent = city.name;
                     option.dataset.lat = city.latitude;
                     option.dataset.lon = city.longitude;
+                    //Después, se manda option como un objeto con atributos hacia el inputRegion(la etiqueta del index.html), y se muestra el nombre de la ciudad gracias a textContent. Se le asignan atributos a option ya que posteriormente se necesita esa información, así que queda guardada.
                     inputRegion.appendChild(option);
                 })
             })
+            //En caso de que algo salga mal, se cambia el option de index.html por Error al cargar ciudades.
             .catch(function(error){
                 console.log("Error al cargar ciudades")
                 inputRegion.innerHTML = '<option value="">— Error al cargar ciudades —</option>';
