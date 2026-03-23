f5e97d865be82263ce376f06f7bc185f416ea078
SantiBetan <santiago.betancur65860@ucaldas.edu.co>
2026-03-23 16:57:27 -0500

    alt shift f

--- js/ui/GridRenderer.js
+++ js/ui/GridRenderer.js
@@ -13,35 +13,35 @@ class GridRenderer {
         "U1": "../../assets/icons/Power-plant.png",
         "U2": "../../assets/icons/Water-plant.png",
         "P1": "../../assets/icons/Park.png",
-        "R":  "../../assets/icons/Road.png"
+        "R": "../../assets/icons/Road.png"
     };
 
     static render(grid, container) {
-    const cellSize = 60;
+        const cellSize = 60;
 
-    container.style.display = "grid";
-    container.style.gridTemplateColumns = `repeat(${grid.width}, ${cellSize}px)`;
-    container.style.gridTemplateRows = `repeat(${grid.height}, ${cellSize}px)`;
-    container.innerHTML = "";
+        container.style.display = "grid";
+        container.style.gridTemplateColumns = `repeat(${grid.width}, ${cellSize}px)`;
+        container.style.gridTemplateRows = `repeat(${grid.height}, ${cellSize}px)`;
+        container.innerHTML = "";
 
-    for (let row = 0; row < grid.height; row++) {
-        for (let col = 0; col < grid.width; col++) {
-            const cell = grid.cells[row][col];
+        for (let row = 0; row < grid.height; row++) {
+            for (let col = 0; col < grid.width; col++) {
+                const cell = grid.cells[row][col];
 
-            const div = document.createElement("div");
-            div.classList.add("cell");
-            div.dataset.x = col;
-            div.dataset.y = row;
+                const div = document.createElement("div");
+                div.classList.add("cell");
+                div.dataset.x = col;
+                div.dataset.y = row;
 
-            if (cell._id !== "g") {
-                const img = GridRenderer.buildingImages[cell._id];
-                if (img) {
-                    div.innerHTML = `<img src="${img}" class="cell-icon"/>`;
+                if (cell._id !== "g") {
+                    const img = GridRenderer.buildingImages[cell._id];
+                    if (img) {
+                        div.innerHTML = `<img src="${img}" class="cell-icon"/>`;
+                    }
                 }
-            }
 
-            container.appendChild(div);
+                container.appendChild(div);
+            }
         }
     }
-}
 }
